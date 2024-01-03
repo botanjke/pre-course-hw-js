16169a7b3d619d5835e02239ff189bbf2bdea998
Pavel <115949945+botanjke@users.noreply.github.com>
2024-01-03 21:38:47 +0300

    Update 05.js

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,12 +1,14 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+let test
 switch (rule) { 
  case rule.length < 25: 
-  console.log("Все таки нет правил без исключения"); 
+  test = "Все таки нет правил без исключения"; 
   break; 
 case rule.length == 25:
-  console.log("50 на 50");
+  test = "50 на 50";
   break;
 case rule.length > 25:
-  console.log("У меня 100 пудов все получится");
+  test = "У меня 100 пудов все получится";
   break;
 };
+console.log(test);
