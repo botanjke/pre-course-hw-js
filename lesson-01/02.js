ffe0926c46422a793dc6167483de5f2437c55bdf
Pavel <develop08@mail.ru>
2024-01-03 17:00:57 +0300

    Update 02.js

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,6 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
-const test = number1 / 2;
-if (test % 2 == 0) {
+if (number1 % 2 == 0) {
     return console.log(`${number1} :Это четное число`)
 } else {
     return console.log(`${number1} :Это нечетное число`)
