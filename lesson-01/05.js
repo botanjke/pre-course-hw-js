41891648f85a20c7913bdc184537cbbef3e6a7d0
Pavel <develop08@mail.ru>
2024-01-03 19:06:23 +0300

    Update 05.js

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -3,10 +3,10 @@ switch (rule) {
  case rule.length < 25: 
   console.log("Все таки нет правил без исключения"); 
   break; 
-  case rule.length == 25:
+case rule.length == 25:
   console.log("50 на 50");
   break;
-  case rule.length > 25:
+case rule.length > 25:
   console.log("У меня 100 пудов все получится");
   break;
 };
