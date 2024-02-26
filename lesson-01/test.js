d86ed3442dd846c94edb8d7b0304f000e9149df5
Pavel <develop08@mail.ru>
2024-02-27 00:25:21 +0300

    Update test.js

--- lesson-01/test.js
+++ lesson-01/test.js
@@ -1,11 +1,11 @@
-     let rule = 777;
-switch (true) { 
-    case rule.length > 25: 
-    console.log("У меня 100 пудов все получится"); 
-    break;
-    case rule.length < 25: 
-        console.log("Все таки нет правил без исключения"); 
-        break; 
-    default: console.log("50 на 50"); 
-        break; 
-}
+const numbers = [ 
+    [1, 2, 3, 4, 5], 
+    [10, 20, 30, 40, 50], 
+    [100, 200, 300, 400, 500], 
+]; 
+ 
+numbers.map(row => console.log(row)) 
+ 
+numbers.map(row => { 
+    row.map(number => console.log(number)); 
+}); 
